eea56a3b348185d2e32d45b346572b260d1f3b9f
tien.dang <tien.dang@viajsc.local>
2024-12-09 18:18:32 +0700

    add document type and scriptsql

---
+++ App_Code/ViewComponents/HeaderViewComponent.cs
@@ -0,0 +1,41 @@
+﻿using Blog.App_Code.Models.ViewComponentModels;
+using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Core.Models.PublishedContent;
+using Umbraco.Cms.Core.Web;
+
+namespace Blog.App_Code.View_Components
+{
+    [ViewComponent]
+    public class HeaderViewComponent : ViewComponent
+    {
+        private readonly ILogger<HeaderViewComponent> _logger;
+        private readonly IUmbracoContextAccessor _context;
+
+        public HeaderViewComponent(ILogger<HeaderViewComponent> logger,
+            IUmbracoContextAccessor context)
+        {            
+            _logger = logger;
+            _context = context;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            HeaderView headerView = new();
+            try
+            {
+                var content = _context?.GetRequiredUmbracoContext()?.PublishedRequest?.PublishedContent;
+
+                if (content is null) return View(headerView);
+
+                headerView.Title = content?.Value<string>("Title");
+                headerView.SubTitle = content?.Value<string>("SubTitle");
+                headerView.ImageUrl = content?.Value<IPublishedContent>("PageBanner")?.Url();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Error while processing {nameof(HeaderViewComponent)}");
+            }
+            return View(headerView);
+        }
+    }
+}
---
+++ App_Code/ViewComponents/MetaDataViewComponent.cs
@@ -0,0 +1,41 @@
+﻿using Blog.App_Code.Models.ViewComponentModels;
+using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Core.Models.PublishedContent;
+using Umbraco.Cms.Core.Web;
+
+namespace Blog.App_Code.ViewComponents
+{
+    [ViewComponent]
+    public class MetaDataViewComponent : ViewComponent
+    {
+        private readonly ILogger<MetaDataViewComponent> _logger;
+        private readonly IUmbracoContextAccessor _context;
+
+        public MetaDataViewComponent(ILogger<MetaDataViewComponent> logger,
+            IUmbracoContextAccessor context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            MetaDataView metaDataView = new();
+            try
+            {
+                var content = _context?.GetRequiredUmbracoContext()?.PublishedRequest?.PublishedContent;
+
+                if (content == null) return View();
+
+                metaDataView.Author = content.Value<string>("Author");
+                metaDataView.Description = content.Value<string>("Description");
+                metaDataView.Title = content.Value<string>("Title");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Error While Processing {nameof(MetaDataViewComponent)}");
+            }
+            return View(metaDataView);
+        }
+    }
+}
